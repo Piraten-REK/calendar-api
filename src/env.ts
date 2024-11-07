588cdf998bcad4917591bdb13cbecab5063996e9
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-11-07 15:34:45 +0100

    add env file

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  PORT: z.string()
+    .transform(port => Number.parseInt(port)).pipe(
+      z.number().int().min(0).max(65535)
+    )
+}).passthrough()
+
+export default envSchema.parse(process.env)
