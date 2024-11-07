dc3bf5183bb4a7ae25657743ff18b8002a0b818f
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-11-07 15:54:48 +0100

    begin adding server

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
 import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 
 const dev = process.env.ENVIRONMENT === 'DEVELOPMENT'
 
@@ -9,15 +11,17 @@ export default [
     input: './src/app.ts',
     output: {
       dir: './dist/',
-      format: 'esm',
+      format: 'cjs',
       sourcemap: true
     },
     plugins: [
+      commonjs(),
       nodeResolve(),
+      json(),
       typescript({
         tsconfig: './tsconfig.json'
       }),
-      dev ? terser() : null
+      dev ? null : terser()
     ]
   }
 ]
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import express from 'express'
+import env from './env'
+
+const app = express()
+
+app.disable('x-powered-by')
+
+app.use('/', (req, res) => {
+  res.end('Hello world')
+})
+
+app.listen(env.PORT, () => {
+  console.log(`Server listening at http://[::1]:${env.PORT}`)
+})
