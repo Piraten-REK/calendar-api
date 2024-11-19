ce2a90340b2bbf97e81688bcdac765a7f9fdef55
Mike Kühnapfel <16109180+veyxos@users.noreply.github.com>
2024-11-19 10:48:00 +0100

    add helpers for ZodErrors

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,7 @@
+import { Response } from 'express'
+import { ProblemObj } from './types'
+import { ZodError } from 'zod'
+
 export function mapGetOrSet <K, V> (map: Map<K, V>, key: K, setter: (() => V)): V {
   if (!map.has(key)) {
     const value = setter()
@@ -7,4 +11,23 @@ export function mapGetOrSet <K, V> (map: Map<K, V>, key: K, setter: (() => V)): 
   }
 
   return map.get(key) as V
+}
+
+export function sendProblem (res: Response, problem: ProblemObj): void {
+  res
+    .status(problem.status)
+    .setHeader('Content-Type', 'application/problem+json; charset=utf-8')
+    .json(problem)
+}
+
+export function sendZodError (res: Response, error: ZodError): void {
+  const [detail] = Object.values(error.flatten())
+    .flat()
+    .filter(it => it != null && typeof it === 'string')
+
+  sendProblem(res, {
+    status: 400,
+    title: error.message,
+    detail
+  })
 }
